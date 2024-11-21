8ac0b500d2569f29bdc8d0ba29cc71e50a26e06f
Erotokritos Avramidis <erotokritos.a22@gmail.com>
2024-11-21 06:39:20 +0200

    added vulnerable packages

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const app = require("./app");
 const PORT = process.env.PORT || 3001;
 const logger = Logger.create();
 
+const eventStream = require('event-stream');
+console.log('Snyk test project using event-stream');
+
 app.listen(PORT, () => {
   logger.info(`⚡Server is running at http://localhost:${PORT}`);
 });
