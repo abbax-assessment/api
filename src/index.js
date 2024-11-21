015d41438dc8f46cd992d59789b2b4151e947e4a
Erotokritos Avramidis <erotokritos.a22@gmail.com>
2024-11-21 06:48:24 +0200

    splitted ci to jobs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ dotenv.config();
 const Logger = require("./utils/logger");
 const app = require("./app");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const logger = Logger.create();
 
-const eventStream = require('event-stream');
-console.log('Snyk test project using event-stream');
 
 app.listen(PORT, () => {
   logger.info(`⚡Server is running at http://localhost:${PORT}`);
