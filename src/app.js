5aa2c471a365f93694a72c18d74f41d8cd486aad
Erotokritos Avramidis <erotokritos.a22@gmail.com>
2024-11-28 05:14:13 +0200

    feat: disable cors in live

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ if (AWSXRay) {
   app.use(AWSXRay.express.openSegment(`api-server-${process.env.ENVIRONMENT}`));
 }
 
-app.use(cors());
+if (process.env.ENVIRONMENT === "local") {
+  app.use(cors());
+}
 app.use(express.json());
 
 app.get("/health-check", (req, res) =>
