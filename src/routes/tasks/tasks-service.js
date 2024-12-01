84d9318cff6fa5d5afc0f83ed8bcb49f2ee79b32
Erotokritos Avramidis <erotokritos.a22@gmail.com>
2024-12-01 05:35:59 +0200

    fix: push uncommented code

--- src/routes/tasks/tasks-service.js
+++ src/routes/tasks/tasks-service.js
@@ -1,8 +1,8 @@
 const { Producer } = require("sqs-producer")
 
-// const producer = Producer.create({
-//   queueUrl: process.env.TASKS_QUEUE_URL
-// });
+const producer = Producer.create({
+  queueUrl: process.env.TASKS_QUEUE_URL
+});
 
 const { v4: uuid } = require("uuid");
 const getXray = require("../../utils/xray");
@@ -23,18 +23,18 @@ class TasksService {
       segment.addAnnotation("Environment", process.env.ENVIRONMENT);
       segment.addMetadata("TaskPayload", tasks);
     }
-    // await producer.send(tasks.map((task) => {
-    //   const taskId = uuid();
-    //   const payload = {
-    //     id: taskId,
-    //     body: JSON.stringify({
-    //       taskBody: { id: taskId, ...task }
-    //     })
-    //   }      
+    await producer.send(tasks.map((task) => {
+      const taskId = uuid();
+      const payload = {
+        id: taskId,
+        body: JSON.stringify({
+          taskBody: { id: taskId, ...task }
+        })
+      }      
 
-    //   newTasks.push(payload);
-    //   return payload;
-    // }))
+      newTasks.push(payload);
+      return payload;
+    }))
 
     return newTasks;
   }
